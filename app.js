f991d51fe6819e3ec7ff177ec69d628dbf08080d
Omar Hady <ohady1992@gmail.com>
2022-03-16 11:16:59 +0200

    Finished UI essential class

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Add A book Class
-class book {
+class Book {
     constructor(title, author, isbn, recby) {
         this.title = title;
         this.author = author;
@@ -13,15 +13,22 @@ class book {
 class UI {
     static DisplayBooks() {
         const books = [{
-            title: "title 1",
-            author: "author 1",
-            isbn: "12345",
-            recby: "Someone 1"
-        }];
+                title: "title 1",
+                author: "author 1",
+                isbn: "12345",
+                recby: "Someone 1"
+            },
+            {
+                title: "title 1",
+                author: "author 1",
+                isbn: "12345",
+                recby: "Someone 1"
+            }
+        ];
         const storedbooks = books;
         storedbooks.forEach((book) => UI.AddBookToList(book));
 
-    };
+    }
     static AddBookToList(book) {
         const list = document.querySelector("#booklist");
         const row = document.createElement('tr');
@@ -30,11 +37,54 @@ class UI {
         <td>${book.author}</td>
         <td>${book.isbn}</td>
         <td>${book.recby}</td>
-        <td><a href="#">X</a></td>`;
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
 
         list.appendChild(row);
 
+
+    }
+    static ClearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+        document.querySelector('#recom').value = '';
+    }
+
+    static DeleteBook(el) {
+        el.parentElement.parentElement.remove();
     }
 
 
 }
+
+
+
+//Event Listeners
+
+// Event listener to grab submissions   
+document.addEventListener('DOMContentLoaded', UI.DisplayBooks);
+//Submission
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+
+    //Prevent default
+    e.preventDefault();
+
+    //Grab Values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+    const recby = document.querySelector('#recom').value;
+
+    //Instantiate book
+    const book = new Book(title, author, isbn, recby);
+
+    UI.AddBookToList(book);
+    UI.ClearFields();
+});
+
+//Remove A book
+
+document.querySelector("#booklist").addEventListener('click', (e) => {
+    UI.DeleteBook(e.target);
+})
